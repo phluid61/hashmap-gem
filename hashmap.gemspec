87e26e0061fe1bc35f6a7d35d721ff61854b75af
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2019-02-07 12:01:52 +1000

    use transform_* methods where available
    bump date/version info for release

--- hashmap.gemspec
+++ hashmap.gemspec
@@ -1,12 +1,17 @@
 Gem::Specification.new do |s|
-  s.name = 'hashmap'
-  s.version = '0.2.0'
-  s.date    = '2016-08-15'
-  s.summary = 'New mappings for Hash'
-  s.description = 'Defines some new mapping methods for Hash.  See https://bugs.ruby-lang.org/issues/7793'
-  s.authors = ['Matthew Kerwin']
-  s.email   = 'matthew@kerwin.net.au'
-  s.files   = ['lib/hashmap.rb']
-  s.homepage = 'http://rubygems.org/gems/hashmap'
-  s.license = 'ISC'
+  s.name      = 'hashmap'
+  s.version   = '1.0.1-dev'
+  s.date      = '2019-01-07'
+  s.summary   = 'New mapping methods for Hash'
+  s.description = <<EOS
+Defines some new mapping methods for Hash.
+
+See: https://bugs.ruby-lang.org/issues/7793
+EOS
+  s.authors   = ['Matthew Kerwin']
+  s.email     = 'matthew@kerwin.net.au'
+  s.files     = ['lib/hashmap.rb']
+  s.test_files= ['test/test_hashmap.rb']
+  s.homepage  = 'https://rubygems.org/gems/hashmap'
+  s.license   = 'ISC'
 end
--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,66 +1,84 @@
 
+if RUBY_VERSION >= '2.4'
+  class Hash
+    alias map_values  transform_values
+    alias map_values! transform_values!
+  end
+else
+  class Hash
+    #
+    # Returns a new hash which is a copy of the current hash but each
+    # value is replaced by the result of running it through +block+.
+    #
+    #   {'a'=>1, 'b'=>2}.map_values { |v| v*2 } #=> {'a'=>2, 'b'=>4}
+    #   {'a'=>1, 'b'=>2}.map_values { "cat" }   #=> {'a'=>"cat", 'b'=>"cat"}
+    #
+    # If no block is given, an Enumerator is returned instead.
+    #
+    def map_values &block # :yields: value
+      return enum_for(:map_values) unless block_given?
+      hsh = {}
+      each do |k, v|
+        hsh[k] = yield v
+      end
+      hsh
+    end
+
+    #
+    # Replaces the values in +hsh+ by running them each through +block+.
+    #
+    # See: #map_values
+    #
+    def map_values! &block # :yields: value
+      return enum_for(:map_values!) unless block_given?
+      replace map_values(&block)
+    end
+  end
+end
+
+if RUBY_VERSION >= '2.5'
+  class Hash
+    alias map_keys  transform_keys
+    alias map_keys! transform_keys!
+  end
+else
+  class Hash
+    #
+    # Returns a new hash which is a copy of the current hash but each
+    # key is replaced by the result of running it through +block+.
+    #
+    # If +block+ returns duplicate keys, they will be overwritten in
+    # the resulting hash.
+    #
+    #   {'a'=>1, 'b'=>2}.map_keys { |k| k*2 } #=> {'aa'=>1, 'bb'=>2}
+    #   {'a'=>1, 'b'=>2}.map_keys { "cat" }   #=> {'cat'=>2}
+    #
+    # If no block is given, an Enumerator is returned instead.
+    #
+    def map_keys &block # :yields: key
+      return enum_for(:map_keys) unless block_given?
+      hsh = {}
+      each do |k, v|
+        hsh[ yield k ] = v
+      end
+      hsh
+    end
+
+    #
+    # Replaces the keys in +hsh+ by running them each through +block+.
+    #
+    # If +block+ returns duplicate keys, they will be overwritten in turn.
+    #
+    # See: #map_keys
+    #
+    def map_keys! &block # :yields: key
+      return enum_for(:map_keys!) unless block_given?
+      replace map_keys(&block)
+    end
+  end
+end
+
 class Hash
-  #
-  # Returns a new hash which is a copy of the current hash but each
-  # value is replaced by the result of running it through +block+.
-  #
-  #   {'a'=>1, 'b'=>2}.map_values { |v| v*2 } #=> {'a'=>2, 'b'=>4}
-  #   {'a'=>1, 'b'=>2}.map_values { "cat" }   #=> {'a'=>"cat", 'b'=>"cat"}
-  #
-  # If no block is given, an Enumerator is returned instead.
-  #
-  def map_values &block # :yields: value
-    return enum_for(:map_values) unless block_given?
-    hsh = {}
-    each do |k, v|
-      hsh[k] = yield v
-    end
-    hsh
-  end
-
-  #
-  # Replaces the values in +hsh+ by running them each through +block+.
-  #
-  # See: #map_values
-  #
-  def map_values! &block # :yields: value
-    return enum_for(:map_values!) unless block_given?
-    replace map_values(&block)
-  end
-
-  #
-  # Returns a new hash which is a copy of the current hash but each
-  # key is replaced by the result of running it through +block+.
-  #
-  # If +block+ returns duplicate keys, they will be overwritten in
-  # the resulting hash.
-  #
-  #   {'a'=>1, 'b'=>2}.map_keys { |k| k*2 } #=> {'aa'=>1, 'bb'=>2}
-  #   {'a'=>1, 'b'=>2}.map_keys { "cat" }   #=> {'cat'=>2}
-  #
-  # If no block is given, an Enumerator is returned instead.
-  #
-  def map_keys &block # :yields: key
-    return enum_for(:map_keys) unless block_given?
-    hsh = {}
-    each do |k, v|
-      hsh[ yield k ] = v
-    end
-    hsh
-  end
-
-  #
-  # Replaces the keys in +hsh+ by running them each through +block+.
-  #
-  # If +block+ returns duplicate keys, they will be overwritten in turn.
-  #
-  # See: #map_keys
-  #
-  def map_keys! &block # :yields: key
-    return enum_for(:map_keys!) unless block_given?
-    replace map_keys(&block)
-  end
-
   #
   # Returns a new hash which is a copy of the current hash but each
   # key-value pair is replaced by the result of running it through +block+.
@@ -95,7 +113,7 @@ class Hash
 end
 
 =begin
-Copyright (c) 2013-2016, Matthew Kerwin <matthew@kerwin.net.au>
+Copyright (c) 2019, Matthew Kerwin <matthew@kerwin.net.au>
 
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
